7398d15e31b3232b636c8a308f6157bf8b7d5e37
El _yahyaouy _Imane <110742439+ELYAHYAOUY-FL@users.noreply.github.com>
2024-08-23 15:19:18 +0100

    Update webSocketServer.py

--- webSocketServer.py
+++ webSocketServer.py
@@ -1,27 +1,30 @@
 import asyncio
 import websockets
 import subprocess
+import socket
 
-async def handle_stream_request(websocket, path):
-    async for message in websocket:
-        print(f"Received message: {message}")
-        if message.startswith("stream:"):
-            video_path = message.split(":", 1)[1]
-            # Construct the VLC command to stream the video
-            command = [
-                'cvlc',  # Command for VLC
-                video_path,  # Path to the video file
-                '--sout', '#rtp{sdp=rtsp://:8554/stream}',  # Streaming options
-                '--no-sout-all', '--sout-keep'
-            ]
-            # Run the VLC command
-            subprocess.Popen(command)
-            print(f"Started streaming {video_path}")
+async def handle_stream_request():
+    # Get the Raspberry Pi's IP address
+    raspberry_pi_ip = socket.gethostbyname(socket.gethostname())
+    uri = f"ws://localhost:5189/videoStreamHub?raspberryPiIp={raspberry_pi_ip}"
+
+    async with websockets.connect(uri) as websocket:
+        while True:
+            message = await websocket.recv()
+            print(f"Received message: {message}")
+            if message.startswith("ReceiveStream:"):
+                video_path = message.split(":", 1)[1]
+                command = [
+                    'cvlc',
+                    video_path,
+                    '--sout', '#rtp{sdp=rtsp://:8554/stream}',
+                    '--no-sout-all', '--sout-keep'
+                ]
+                subprocess.Popen(command)
+                print(f"Started streaming {video_path}")
 
 async def main():
-    server = await websockets.serve(handle_stream_request, '0.0.0.0', 8765)
-    print("WebSocket server started on port 8765")
-    await server.wait_closed()
+    await handle_stream_request()
 
 if __name__ == "__main__":
     asyncio.run(main())
