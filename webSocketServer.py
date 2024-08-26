69bf035c287a0b028ba5b84300d973096cab732b
El _yahyaouy _Imane <110742439+ELYAHYAOUY-FL@users.noreply.github.com>
2024-08-26 14:37:44 +0100

    Update webSocketServer.py

--- webSocketServer.py
+++ webSocketServer.py
@@ -3,23 +3,19 @@ import subprocess
 
 app = Flask(__name__)
 
-# Store the process ID (PID) of the VLC instance
-vlc_process = None
 
+vlc_process = None
 @app.route('/stream', methods=['POST'])
 def stream_video():
     global vlc_process
     data = request.json
     video_url = data.get('video_url')
-
     if not video_url:
         return jsonify({'error': 'No video URL provided'}), 400
-
     # Stop any existing VLC process
     if vlc_process and vlc_process.poll() is None:
         vlc_process.terminate()
         vlc_process.wait()
-
     # VLC command to stream the video
     vlc_command = f"cvlc --fullscreen --play-and-exit {video_url}"
     
